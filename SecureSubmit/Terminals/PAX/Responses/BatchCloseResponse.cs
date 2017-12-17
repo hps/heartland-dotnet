13c5df6f8ce4b734daac3ddaf5580c78b81d7073
Sean Fuhrmann <sean.fuhrmann@posail.com>
2017-12-16 18:34:29 -0600

    Added HostResposne to public property on BatchCloseResponse.  Also parsed Total properties - NOTE this would be a breaking change.

--- SecureSubmit/Terminals/PAX/Responses/BatchCloseResponse.cs
+++ SecureSubmit/Terminals/PAX/Responses/BatchCloseResponse.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using SecureSubmit.Terminals.Extensions;
 
 namespace SecureSubmit.Terminals.PAX {
     public class BatchCloseResponse : PaxDeviceResponse {
         private HostResponse hostResponse;
 
-        public string TotalCount { get; set; }
-        public string TotalAmount { get; set; }
+        public int TotalCount { get; set; }
+        public decimal TotalAmount { get; set; }
         public string TimeStamp { get; set; }
         public string TID { get; set; }
         public string MID { get; set; }
@@ -20,11 +21,13 @@ namespace SecureSubmit.Terminals.PAX {
             base.ParseResponse(br);
 
             this.hostResponse = new HostResponse(br);
-            this.TotalCount = br.ReadToCode(ControlCodes.FS);
-            this.TotalAmount = br.ReadToCode(ControlCodes.FS);
+            this.TotalCount = br.ReadToCode(ControlCodes.FS).Split('=').Sum(o => Convert.ToInt32(o));
+            this.TotalAmount = br.ReadToCode(ControlCodes.FS).Split('=').Sum(o => Convert.ToDecimal(o) / 100);
             this.TimeStamp = br.ReadToCode(ControlCodes.FS);
             this.TID = br.ReadToCode(ControlCodes.FS);
             this.MID = br.ReadToCode(ControlCodes.ETX);
+
+            this.HostResponse = hostResponse;
         }
     }
 }
